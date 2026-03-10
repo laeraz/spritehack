2cc7ae43f1f6fd40ceacd01e11b419c99822f237
Laera <130590222+laeraz@users.noreply.github.com>
2026-03-11 00:31:40 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
+local Games = "https://raw.githubusercontent.com/laeraz/spritehack/refs/heads/main/games/"
+local PlaceId = game.PlaceId
 
+if PlaceId == 114234929420007 then
+    loadstring(game:HttpGet(Games .. "bloxstrike.lua"))()
+end
